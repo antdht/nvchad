c69e99f5a9488dba8356a68960d9d8e101c63537
antdht <gzavier@proton.me>
2025-10-19 19:01:52 +0200

    lazygit integration in nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -89,4 +89,20 @@ return {
     --   require "configs.todo"
     -- end,
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    }
+  },
 }
