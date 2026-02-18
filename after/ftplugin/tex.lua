d0497ad8152d0f99a8bb920e68a9d7876cb173ac
antdht <antdht@proton.me>
2026-02-18 21:03:21 +0100

    fixed conflicts

--- after/ftplugin/tex.lua
+++
@@ -1,7 +0,0 @@
-local map = vim.keymap.set
-
-vim.opt_local.spell = true
-vim.opt_local.spelllang = { "en_gb", "fr" }
-
--- Quickly correct the last misspelled word
-map("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = false, buffer = true })
--- lua/configs/vimtex.lua
+++ lua/configs/vimtex.lua
@@ -2,7 +2,22 @@
 -- following line. The default is usually fine and is the symbol "\".
 
 vim.g.vimtex_view_method = "mupdf"
+vim.g["vimtex_quickfix_mode"] = 1
 
 vim.g.vimtex_quickfix_ignore_filters = {
   "Package lipsum Warning",
 }
+
+-- init = function()
+--   -- VimTeX configuration goes here, e.g.
+--   vim.g.vimtex_view_method = "mupdf"
+--   vim.g["vimtex_quickfix_mode"] = 1
+--   vim.g.maplocalleader = "\\"
+--   vim.g["vimtex_log_ignore"] = { -- Error suppression:
+--     "Underfull",
+--     "Overfull",
+--     "specifier changed to",
+--     "Token not allowed in a PDF string",
+--     "Package caption Warning: The option `hypcap=true' will be ignored",
+--   }
+-- end,
